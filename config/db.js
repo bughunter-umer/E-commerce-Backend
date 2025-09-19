9ef56fbe730930f5f5a0cdaf8beb49b92b5ba3ae
Umer <umerawan19791@gmail.com>
2025-09-19 19:40:49 +0500

    updated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,25 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("ecommerce", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-});
+let sequelize;
+
+if (process.env.NODE_ENV === "production") {
+  // 👉 Railway (Production)
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for Railway
+      },
+    },
+  });
+} else {
+  // 👉 Local Development
+  sequelize = new Sequelize("ecommerce", "postgres", "postgres", {
+    host: "localhost",
+    dialect: "postgres",
+  });
+}
 
 export default sequelize;
